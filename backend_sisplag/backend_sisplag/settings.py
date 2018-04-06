30a1e74b8fa8c6273639a961fe4ec9a8caee2025
Daniel Gazcon <daniel.gazcon27@gmail.com>
2018-04-05 23:37:00 -0400

    including rest django

--- backend_sisplag/backend_sisplag/settings.py
+++ backend_sisplag/backend_sisplag/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '%^i9ux8hjjbsy!n2nm(o*7ul0-d=2z6hj4c_jw-e*)p01^8vkj'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -118,3 +118,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+try:
+    from backend_sisplag.local_settings import *
+except Exception as e:
+    raise
