8a0d19b3409b4ed4a6a2c0971fa42df156aefe12
Daniel Gazcon <daniel.gazcon27@gmail.com>
2018-05-11 16:00:29 -0400

    upload document

--- loopback-backend/common/models/document.js
+++ loopback-backend/common/models/document.js
@@ -2,6 +2,7 @@
 
 module.exports = function(Document) {
 
+	var promise = require('promise');
 	var app;
 	Document.on('attached', function (a) {
 		app = a;
@@ -11,13 +12,59 @@ module.exports = function(Document) {
 		var Container = app.models.Container;
 		var AccessToken = app.models.AccessToken;
 		var error;
-		if (process.env.NODE_ENV === 'development') {
-			Container.upload(req, res, { container: 'documents' }, cb)
-		} else {
-			Container.upload(req, res, { container: 'documents' }, cb)
-		}
+		Container.upload(req, res, { container: 'documents' }, cb)
 	}
 	/* Remote Methods */
+
+	Document.readDocument = function (cb) {
+		var fs = require("fs");
+		var pdfreader = require('pdfreader');
+		var docu = '/home/deadline004/Documents/Daniel/Code/storage/documents/text_23445345.pdf'
+		var i = 0
+		var rows = {}
+		new pdfreader.PdfReader().parseFileItems(docu, function(err, item){
+			if (item == undefined) {
+				// finish read document
+				printRows(rows)
+				.then(r=>{
+					var response = {text:r}
+					console.log(response)
+					cb(null,response)
+				})
+				.catch(e=>cb(e, null))
+			} else if(item.text) {
+				// accumulate text items into rows object, per line
+				(rows[item.y] = rows[item.y] || []).push(item.text);
+			}
+			
+		});
+	}
+
+	function printRows(rows) {
+		var defer = new promise((resolve, reject)=> {
+			var text = ''
+			Object.keys(rows) // => array of y-positions (type: float)
+			.sort((y1, y2) => parseFloat(y1) - parseFloat(y2)) // sort float positions
+			.forEach((y) => {
+				text += (rows[y] || []).join('')
+				// console.log((rows[y] || []).join(''))
+			});
+			resolve(text)
+		})
+		return defer;
+	}
+
+	Document.remoteMethod('readDocument', {
+		http: {
+			path: '/read-document',
+			verb: 'get'
+		},
+		accepts: [],
+		return: {
+			arg: 'object',
+			root:true
+		}
+	});
 
 	Document.remoteMethod('upload',{
 		http: { 
